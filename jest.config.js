5aeccc61f467cb2ff85a5b87de2c6ca960a02a80
Andrew Downing <andrew.c.downing@gmail.com>
2022-11-11 22:50:30 -0700

    add: generate test reports for code build

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: 'test_reports',
+      outputName: 'test_report.xml',
+    } ]
+  ]
 };
