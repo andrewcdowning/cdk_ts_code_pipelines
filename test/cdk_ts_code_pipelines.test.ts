3b7757642740ead41325e5bb78557d3fcf620e04
Andrew Downing <andrew.c.downing@gmail.com>
2022-11-13 18:32:34 -0700

    add: test add stage method

--- test/cdk_ts_code_pipelines.test.ts
+++ test/cdk_ts_code_pipelines.test.ts
@@ -1,6 +1,7 @@
 import { App, StackSynthesizer } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { PipelinesStack } from "../lib/pipelines-stack";
+import { ServiceStack } from "../lib/service-stack";
 
 test('Pipeline', () => {
     const app = new App();
@@ -9,4 +10,22 @@ test('Pipeline', () => {
     const template = Template.fromStack(stack);
     expect(template.toJSON()).toMatchSnapshot();
 
+});
+
+test('Adding service stage', () => {
+    const app = new App();
+    const serviceStack = new ServiceStack(app, 'ServiceStack', {});
+    const pipelineStack = new PipelinesStack(app, 'PipelineStack');
+
+    pipelineStack.addServiceStage(serviceStack, 'testStack');
+
+    const template = Template.fromStack(pipelineStack);
+    expect(template.hasResourceProperties("AWS::CodePipeline::Pipeline", {
+        "Stages": [
+            {}, {}, {},
+            {
+                "Name": 'testStack'
+            }
+        ]
+    }));
 });
