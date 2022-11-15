90a6aa32b7a93beb7ecc6ce6bba74845ab527161
Andrew Downing <andrew.c.downing@gmail.com>
2022-11-14 19:43:53 -0700

    add: service stack endpoint for tests

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,6 +1,7 @@
 import { HttpApi, HttpIntegration } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha"
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutcome } from "aws-cdk-lib/aws-frauddetector";
 import { CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
@@ -12,6 +13,7 @@ export interface ServiceStackProps extends StackProps{
 export class ServiceStack extends Stack {
 
     public serviceCode: CfnParametersCode;
+    public serviceEndpointOutput: CfnOutput;
 
     constructor(scope: Construct, id: string, props: ServiceStackProps){
         super(scope, id, props);
@@ -28,6 +30,12 @@ export class ServiceStack extends Stack {
         const proxyApi = new HttpApi(this, 'ServiceProxyApi', {
             defaultIntegration: new HttpLambdaIntegration('LambdaIntegration', lambdaFunction),
             apiName: `ServiceApi-${props.stageName}`
+        });
+
+        this.serviceEndpointOutput = new CfnOutput(this, 'ApiEndpointOutput', {
+            exportName: `ServiceEndpoint${props.stageName}`,
+            value: proxyApi.apiEndpoint,
+            description: "API Endpoint"
         });
     }
 }
